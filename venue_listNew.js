295990cf5fd7a74abeee01b2b6c3ea749b52b016
Yogeshwar patil <pyogeshwar963@gmail.com>
2024-11-29 15:56:54 +0530

    commit changes

---
+++ venue_listNew.js
@@ -0,0 +1,478 @@
+const venues = [
+  {
+    name: 'Yoddha Sports Club',
+    rating: 4,
+    deferLink: 'https://playo.page.link/ry8TT',
+    fullLink:
+      'https://playo.co/venue/?venueId=4ec5b58f-d58f-4ce1-8c84-2caa63007ecc',
+    avgRating: 4,
+    ratingCount: 3.6,
+    lat: 18.635119880729967,
+    lng: 73.7346085902281,
+    icon: 'https://maps.google.com/mapfiles/kml/paddle/4-lv.png',
+    filter_by: ['Pool', 'Snooker'],
+    sportsAvailable: [
+      {
+        id: '10',
+        name: 'Badminton',
+        icon: 'badminton',
+        price: 500,
+        courts: [
+          {
+            id: '10',
+            name: 'Standard synthetic court 1',
+            number: 1,
+          },
+          {
+            id: '11',
+            name: 'Standard synthetic court 2',
+            number: 2,
+          },
+          {
+            id: '12',
+            name: 'Standard synthetic court 3',
+            number: 3,
+          },
+        ],
+      },
+
+      {
+        id: '11',
+        name: 'Cricket',
+        icon: 'cricket',
+        price: 1100,
+        courts: [
+          {
+            id: '10',
+            name: 'Full Pitch 1',
+            number: 1,
+          },
+          {
+            id: '11',
+            name: 'Full Pitch 2',
+            number: 2,
+          },
+        ],
+      },
+      {
+        id: '12',
+        name: 'Tennis',
+        icon: 'tennis',
+        price: 900,
+        courts: [
+          {
+            id: '10',
+            name: 'Court 1',
+            number: 1,
+          },
+          {
+            id: '11',
+            name: 'Court 2',
+            number: 2,
+          },
+        ],
+      },
+    ],
+    image:"https://playo.gumlet.io/FIGURINEFITNESSINDIRANAGAR/SnookerRoom1652349575145.jpeg?mode=crop&crop=smart&h=200&width=450&q=75",
+    location:
+      'Behind Adhira international school, Divine bliss rd Sr. No. 38/4, Jambe Rd, Tajane wasti Punawale',
+    address:"Punawale",
+    bookings: [],
+  },
+  {
+    name: 'Game Point Arena',
+    rating: 4,
+    deferLink: "https://z34v4.app.goo.gl/MAAX",
+    fullLink: "https://playo.co/venue/?venueId=afbe7186-2f86-4215-8715-4b967f166b09",
+    avgRating: 4,
+    ratingCount: 3,
+    lat: 18.60139387648086,
+    lng: 73.76740839556835,
+    icon: 'https://maps.google.com/mapfiles/kml/paddle/4-lv.png',
+    filter_by: ['Pool', 'Snooker'],
+    sportsAvailable: [
+      {
+        id: '10',
+        name: 'Badminton',
+        icon: 'badminton',
+        price: 500,
+        courts: [
+          {
+            id: '10',
+            name: 'Standard synthetic court 1',
+            number: 1,
+          },
+          {
+            id: '11',
+            name: 'Standard synthetic court 2',
+            number: 2,
+          },
+          {
+            id: '12',
+            name: 'Standard synthetic court 3',
+            number: 3,
+          },
+        ],
+      },
+
+      {
+        id: '11',
+        name: 'Cricket',
+        icon: 'cricket',
+        price: 1100,
+        courts: [
+          {
+            id: '10',
+            name: 'Full Pitch 1',
+            number: 1,
+          },
+          {
+            id: '11',
+            name: 'Full Pitch 2',
+            number: 2,
+          },
+        ],
+      },
+      {
+        id: '12',
+        name: 'Tennis',
+        icon: 'tennis',
+        price: 900,
+        courts: [
+          {
+            id: '10',
+            name: 'Court 1',
+            number: 1,
+          },
+          {
+            id: '11',
+            name: 'Court 2',
+            number: 2,
+          },
+        ],
+      },
+    ],
+    image:"https://playo.gumlet.io/OVALNETBADMINTONACADEMY/OvalNetBadmintonAcademy6.jpg?mode=crop&crop=smart&h=200&width=450&q=75",
+    location:
+      'Pink City Road Opposite Ashwani Galaxy',
+    address:"Wakad",
+    bookings: [],
+  },
+  {
+    name: 'IT59 Sports Zone',
+    rating: 4,
+    deferLink: "https://z34v4.app.goo.gl/MAAX",
+    fullLink: "https://playo.co/venue/?venueId=afbe7186-2f86-4215-8715-4b967f166b09",
+    avgRating: 4,
+    ratingCount: 3,
+    lat: 18.46713277368824,
+    lng: 73.92380646487815,
+    icon: 'https://maps.google.com/mapfiles/kml/paddle/4-lv.png',
+    filter_by: ['Pool', 'Snooker'],
+    sportsAvailable: [
+      {
+        id: '10',
+        name: 'Badminton',
+        icon: 'badminton',
+        price: 500,
+        courts: [
+          {
+            id: '10',
+            name: 'Standard synthetic court 1',
+            number: 1,
+          },
+          {
+            id: '11',
+            name: 'Standard synthetic court 2',
+            number: 2,
+          },
+          {
+            id: '12',
+            name: 'Standard synthetic court 3',
+            number: 3,
+          },
+        ],
+      },
+
+      {
+        id: '11',
+        name: 'Cricket',
+        icon: 'cricket',
+        price: 1100,
+        courts: [
+          {
+            id: '10',
+            name: 'Full Pitch 1',
+            number: 1,
+          },
+          {
+            id: '11',
+            name: 'Full Pitch 2',
+            number: 2,
+          },
+        ],
+      },
+      {
+        id: '12',
+        name: 'Tennis',
+        icon: 'tennis',
+        price: 900,
+        courts: [
+          {
+            id: '10',
+            name: 'Court 1',
+            number: 1,
+          },
+          {
+            id: '11',
+            name: 'Court 2',
+            number: 2,
+          },
+        ],
+      },
+    ],
+    image:"https://playo.gumlet.io/OVALNETBADMINTONACADEMY/OvalNetBadmintonAcademy6.jpg?mode=crop&crop=smart&h=200&width=450&q=75",
+    location:
+      'Hinjewadi Phase 1 Rd, Near IT59 Food Court, Hinjewadi, Pune, Maharashtra 411057',
+    address:"Hinjewadi",
+    bookings: [],
+  },
+  {
+    name: 'Metro Sports Arena',
+    rating: 4,
+    fullLink: "https://playo.co/venue?venueId=6bb450c0-318b-49e5-b7c0-c02a37d34ef8",
+    deferLink: "https://z34v4.app.goo.gl/4Kqo",
+    avgRating: 4,
+    ratingCount: 3,
+    lat: 18.62463392317798,
+    lng: 73.75114279556897,
+    icon: 'https://maps.google.com/mapfiles/kml/paddle/4-lv.png',
+    filter_by: ['Pool', 'Snooker'],
+    sportsAvailable: [
+      {
+        id: '10',
+        name: 'Badminton',
+        icon: 'badminton',
+        price: 500,
+        courts: [
+          {
+            id: '10',
+            name: 'Standard synthetic court 1',
+            number: 1,
+          },
+          {
+            id: '11',
+            name: 'Standard synthetic court 2',
+            number: 2,
+          },
+          {
+            id: '12',
+            name: 'Standard synthetic court 3',
+            number: 3,
+          },
+        ],
+      },
+
+      {
+        id: '11',
+        name: 'Cricket',
+        icon: 'cricket',
+        price: 1100,
+        courts: [
+          {
+            id: '10',
+            name: 'Full Pitch 1',
+            number: 1,
+          },
+          {
+            id: '11',
+            name: 'Full Pitch 2',
+            number: 2,
+          },
+        ],
+      },
+      {
+        id: '12',
+        name: 'Tennis',
+        icon: 'tennis',
+        price: 900,
+        courts: [
+          {
+            id: '10',
+            name: 'Court 1',
+            number: 1,
+          },
+          {
+            id: '11',
+            name: 'Court 2',
+            number: 2,
+          },
+        ],
+      },
+    ],
+    image:"https://playo.gumlet.io/PLAYZONESAHAKARNAGARSHREEVAYUBADMINTONARENA20231206074712995440/PlayZoneSahakarnagarShreeVayuBadmintonArena1701880566748.jpeg?mode=crop&crop=smart&h=200&width=450&q=75",
+    location:
+      'high street, opp. Austin Yana, Ashok Nagar, Tathawade, Pimpri-Chinchwad',
+    address:"Tathawade",
+    bookings: [],
+  },
+  {
+    name: 'Impetus Sporting Punawale',
+    rating: 4,
+    deferLink: "https://z34v4.app.goo.gl/RTF4",
+    fullLink: "https://playo.co/venue/?venueId=37f3675b-dfd2-4f30-8506-a3883abef902",
+    avgRating: 4,
+    ratingCount: 3,
+    lat: 18.63893214054104,
+    lng: 73.7354323955694,
+    icon: 'https://maps.google.com/mapfiles/kml/paddle/4-lv.png',
+    filter_by: ['Pool', 'Snooker'],
+    sportsAvailable: [
+      {
+        id: '10',
+        name: 'Badminton',
+        icon: 'badminton',
+        price: 500,
+        courts: [
+          {
+            id: '10',
+            name: 'Standard synthetic court 1',
+            number: 1,
+          },
+          {
+            id: '11',
+            name: 'Standard synthetic court 2',
+            number: 2,
+          },
+          {
+            id: '12',
+            name: 'Standard synthetic court 3',
+            number: 3,
+          },
+        ],
+      },
+
+      {
+        id: '11',
+        name: 'Cricket',
+        icon: 'cricket',
+        price: 1100,
+        courts: [
+          {
+            id: '10',
+            name: 'Full Pitch 1',
+            number: 1,
+          },
+          {
+            id: '11',
+            name: 'Full Pitch 2',
+            number: 2,
+          },
+        ],
+      },
+      {
+        id: '12',
+        name: 'Tennis',
+        icon: 'tennis',
+        price: 900,
+        courts: [
+          {
+            id: '10',
+            name: 'Court 1',
+            number: 1,
+          },
+          {
+            id: '11',
+            name: 'Court 2',
+            number: 2,
+          },
+        ],
+      },
+    ],
+    image:"https://playo.gumlet.io/VINI5BADMINTONARENA20240226042742110513/Vini5BadmintonArena1709376498394.jpg?mode=crop&crop=smart&h=200&width=450&q=75",
+    location:
+      'opposite Shreyan Soceity, Rasikwadi, Punawale, Pune, Pimpri-Chinchwad, Maharashtra 411033',
+    address:"Punawale",
+    bookings: [],
+  },
+  {
+    name: 'Ujs Turf',
+    rating: 4,
+    fullLink: "https://playo.co/venue?venueId=a0c6ceb4-d09b-4fcf-bafd-6c949a55590c",
+    deferLink: "https://z34v4.app.goo.gl/3k9a",
+    avgRating: 4,
+    ratingCount: 3,
+    lat: 18.611289670376728,
+    lng: 73.74954790906108,
+    icon: 'https://maps.google.com/mapfiles/kml/paddle/4-lv.png',
+    filter_by: ['Pool', 'Snooker'],
+    sportsAvailable: [
+      {
+        id: '10',
+        name: 'Badminton',
+        icon: 'badminton',
+        price: 500,
+        courts: [
+          {
+            id: '10',
+            name: 'Standard synthetic court 1',
+            number: 1,
+          },
+          {
+            id: '11',
+            name: 'Standard synthetic court 2',
+            number: 2,
+          },
+          {
+            id: '12',
+            name: 'Standard synthetic court 3',
+            number: 3,
+          },
+        ],
+      },
+
+      {
+        id: '11',
+        name: 'Cricket',
+        icon: 'cricket',
+        price: 1100,
+        courts: [
+          {
+            id: '10',
+            name: 'Full Pitch 1',
+            number: 1,
+          },
+          {
+            id: '11',
+            name: 'Full Pitch 2',
+            number: 2,
+          },
+        ],
+      },
+      {
+        id: '12',
+        name: 'Tennis',
+        icon: 'tennis',
+        price: 900,
+        courts: [
+          {
+            id: '10',
+            name: 'Court 1',
+            number: 1,
+          },
+          {
+            id: '11',
+            name: 'Court 2',
+            number: 2,
+          },
+        ],
+      },
+    ],
+    image:"https://playo.gumlet.io/SERVESMASH20191003055000886885/ServeSmash0.jpeg?mode=crop&crop=smart&h=200&width=450&q=75",
+    location:
+      'Mumbai-Bangalore Express Highway, Near Indira College, Tathawade, Pune 411033',
+    address:"Tathawade",
+    bookings: [],
+  },
+  // Add more venues as need
+];
